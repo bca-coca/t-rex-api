66c145bcab0372a5b508de1d6ac436d4eb74fe9b
Atul Singh <cocatul11@gmail.com>
2025-02-04 16:45:56 +0530

    Refactor TeamController to use @ModelAttribute for binding multipart form data

--- src/main/java/trex/com/web/team/controller/TeamController.java
+++ src/main/java/trex/com/web/team/controller/TeamController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,38 +31,33 @@ public class TeamController {
 
     @GetMapping
     public ResponseEntity<List<TeamModel>> getAllTeamMembers() {
-        log.info("Fetching all team members");
         return ResponseEntity.ok(service.getAllTeamMembers());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TeamModel> getTeamMember(@PathVariable Long id) {
-        log.info("Fetching team member with ID: {}", id);
         return ResponseEntity.ok(service.getTeamMember(id));
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<TeamModel> createTeamMember(
-        @RequestPart("member") @Valid TeamModel teamMember,
+        @ModelAttribute @Valid TeamModel teamMember,
         @RequestPart("image") MultipartFile image
     ) {
-        log.info("Creating new team member with image");
         return new ResponseEntity<>(service.addTeamMember(teamMember, image), HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<TeamModel> updateTeamMember(
         @PathVariable Long id,
-        @RequestPart("member") @Valid TeamModel teamMember,
+        @ModelAttribute @Valid TeamModel teamMember,
         @RequestPart(value = "image", required = false) MultipartFile image
     ) {
-        log.info("Updating team member with ID: {}", id);
         return ResponseEntity.ok(service.updateTeamMember(id, teamMember, image));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTeamMember(@PathVariable Long id) {
-        log.info("Deleting team member with ID: {}", id);
         service.deleteTeamMember(id);
         return ResponseEntity.noContent().build();
     }
